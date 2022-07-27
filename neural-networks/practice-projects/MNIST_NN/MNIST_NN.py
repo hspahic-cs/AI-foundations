36de85d436bf897d0f6f078d3e7af557f167fc3f
hspahic-cs <harrisspahic1190@gmail.com>
2022-07-27 15:55:41 -0400

    Added comments to MNST project

--- neural-networks/practice-projects/MNIST_NN/MNIST_NN.py
+++ neural-networks/practice-projects/MNIST_NN/MNIST_NN.py
@@ -2,25 +2,31 @@ from tensorflow import keras
 from keras import layers
 from keras.datasets import mnist
 
+# Load in relevant data
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
-model = keras.Sequential([
-    layers.Dense(512, activation="relu"),
-    layers.Dense(10, activation="softmax")
-])
-
-model.compile(
-    optimizer="rmsprop",
-    loss="sparse_categorical_crossentropy",
-    metrics=["accuracy"])
-
+# Reshaping data  
 train_images = train_images.reshape((60000, 28*28))
 train_images = train_images.astype("float32") / 255
 test_images = test_images.reshape((10000, 28*28))
 test_images = test_images.astype("float32") / 255
 
+# Loading in layers 
+model = keras.Sequential([
+    layers.Dense(512, activation="relu"),
+    layers.Dense(10, activation="softmax")
+])
+
+# Defining loss & optimization methods for model
+model.compile(
+    optimizer="rmsprop",
+    loss="sparse_categorical_crossentropy",
+    metrics=["accuracy"])
+
+# Fit model to data
 model.fit(train_images, train_labels, epochs=5, batch_size=128)
 
+# Test predictions
 test_digits = test_images[0:10]
 predictions = model.predict(test_digits)
 print(predictions[0].argmax())
